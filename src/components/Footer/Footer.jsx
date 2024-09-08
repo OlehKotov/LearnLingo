570666be14daed80ff2fac86cd5d0af610d4068d
OlehKotov <mail.oleg456@gmail.com>
2024-09-08 16:26:12 +0300

    Create Footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import css from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer className={css.footer}>
+      <ul className={css.tutorList}>
+        <li className={css.tutorItem}>
+          <div className={css.tutorCount}>32,000 +</div>
+          <div className={css.tutorName}>Experienced tutors</div>
+        </li>
+        <li className={css.tutorItem}>
+          <div className={css.tutorCount}>300,000 +</div>
+          <div className={css.tutorName}>5-star tutor reviews</div>
+        </li>
+        <li className={css.tutorItem}>
+          <div className={css.tutorCount}>120 +</div>
+          <div className={css.tutorName}>
+            Subjects <br /> taught
+          </div>
+        </li>
+        <li className={css.tutorItem}>
+          <div className={css.tutorCount}>200 +</div>
+          <div className={css.tutorName}>Tutor nationalities</div>
+        </li>
+      </ul>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -5,7 +5,10 @@ const Main = () => {
   return (
     <section className={css.main}>
       <div className={css.getBlock}>
-        <h1 className={css.getBlockHeader}>Unlock your potential with the best <span className={css.getBlockSpan}>language</span> tutors</h1>
+        <h1 className={css.getBlockHeader}>
+          Unlock your potential with the best{" "}
+          <span className={css.getBlockSpan}>language</span> tutors
+        </h1>
         <p className={css.getBlockText}>
           Embark on an Exciting Language Journey with Expert Language Tutors:
           Elevate your language proficiency to new heights by connecting with
@@ -14,8 +17,16 @@ const Main = () => {
         <button className={css.getBlockButton}>Get started</button>
       </div>
       <div className={css.imageBlock}>
-        <img src="/src/assets/images/lion.png" alt="Lion" className={css.imageBlockLion}/>
-        <img src="/src/assets/images/macbook.png" alt="Macbook" className={css.imageBlockMacbook}/>
+        <img
+          src="/src/assets/images/lion.png"
+          alt="Lion"
+          className={css.imageBlockLion}
+        />
+        <img
+          src="/src/assets/images/macbook.png"
+          alt="Macbook"
+          className={css.imageBlockMacbook}
+        />
       </div>
     </section>
   );
